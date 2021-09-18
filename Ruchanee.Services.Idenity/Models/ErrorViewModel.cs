0f991894858b456ecb807ce696e202af995891b0
Dave Harms <daveharmswebdev@gmail.com>
2021-09-18 17:10:15 -0500

    initialized identity server

---
+++ Ruchanee.Services.Idenity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ruchanee.Services.Idenity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
