8b32bd3af84e85bc33bfc347315b8df6ba2fc887
Dave Harms <daveharmswebdev@gmail.com>
2021-09-18 07:09:52 -0500

    added migration of seed proects and finished controller

--- Ruchanee.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Ruchanee.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -45,11 +45,11 @@ namespace Ruchanee.Services.ProductAPI.Controllers
                 {
                     _response.IsSuccess = false;
                     _response.DisplayMessage = "Product id does not exist";
+                    return _response;
                 }
-                else
-                {
-                    _response.Result = productDto;
-                }
+                _response.Result = productDto;
+                _response.IsSuccess = true;
+
             }
             catch (Exception ex)
             {
@@ -58,7 +58,58 @@ namespace Ruchanee.Services.ProductAPI.Controllers
                     new List<string>() { ex.ToString() };
             }
             return _response;
+        }
 
+        [HttpPost]
+        public async Task<object> CreateProduct([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages =
+                    new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        public async Task<object> UpdateProduct([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages =
+                    new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<object> DeleteProduct(int id)
+        {
+            try
+            {
+                var productIsDeleted = await _productRepository.DeleteProduct(id);
+                _response.IsSuccess = productIsDeleted;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages =
+                    new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
     }
 }
