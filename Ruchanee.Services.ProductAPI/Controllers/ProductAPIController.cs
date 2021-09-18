f2af803c2bbf64fe238d6678d58e6e8f16f30213
Dave Harms <daveharmswebdev@gmail.com>
2021-09-18 06:19:55 -0500

    added get and getbyid routes

---
+++ Ruchanee.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ruchanee.Services.ProductAPI.Models.Dtos;
+using Ruchanee.Services.ProductAPI.Repositories;
+
+namespace Ruchanee.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages =
+                    new List<string>() { ex.ToString() };
+            }
+            return _response;
+
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> GetById(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                if (productDto == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.DisplayMessage = "Product id does not exist";
+                }
+                else
+                {
+                    _response.Result = productDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages =
+                    new List<string>() { ex.ToString() };
+            }
+            return _response;
+
+        }
+    }
+}
